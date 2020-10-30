329121485ddafe74a786f02bb9124f9b839ad118
Jose Guillermo <jguillermo@outlook.com>
2020-10-30 11:39:04 -0500

    contador: cards

--- basic/componentes/lib/src/pages/card_page.dart
+++ basic/componentes/lib/src/pages/card_page.dart
@@ -7,6 +7,54 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Card Page'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: [_cardTipo1(), SizedBox(height: 30.0), _cardTipo2()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.access_alarm),
+            title: Text('titulo del card'),
+            subtitle: Text(
+                'este es un texto cualquiera para mostrar data en la demo.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(onPressed: () {}, child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('Ok'))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Column(
+        children: [
+          FadeInImage(
+              placeholder: AssetImage('assets/jar-loading.gif'),
+              image: NetworkImage(
+                  'https://i.ytimg.com/vi/c7oV1T2j5mc/maxresdefault.jpg'),
+              fadeInDuration: Duration(milliseconds: 200),
+              height: 300.0,
+              fit: BoxFit.cover),
+          Container(
+              padding: EdgeInsets.all(10.0), child: Text('imagen paisaje 1'))
+        ],
+      ),
     );
   }
 }

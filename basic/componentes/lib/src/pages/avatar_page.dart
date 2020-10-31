87cc93e8e163dbafab96813e4ababc60702cec21
Jose Guillermo <jguillermo@outlook.com>
2020-10-30 23:23:42 -0500

    contador: avatar

--- basic/componentes/lib/src/pages/avatar_page.dart
+++ basic/componentes/lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.telesurtv.net/export/sites/telesur/img/multimedia/2015/06/17/einsteina1.jpg_96239115.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JJ'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://st-listas.20minutos.es/images/2009-11/163716/1823659_640px.jpg?1519218076'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
